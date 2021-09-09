84874ab43a65da32b89789d7be13d72a89829ebc
sergio5432 <Pinedaruizsergioandres@gmail.com>
2021-09-08 19:08:13 -0500

    trabajando java script
    agregando java script  a nuestro reda

---
+++ ovahtml5/js/index.js
@@ -0,0 +1,7 @@
+alert("HOLA, BIENVENIDOS A SU PAGINA WEB EDUCATIVA");
+Text=prompt("¿HAZ ESCUHADO SOBRE MITOS Y LEYENDAS  ","");
+document.writeln("SU RESPUESTA ES: "+Text);
+var nombre = prompt ("DIGITE SUS NOMBRES: ","")
+var apellidos = prompt ("DIGITE SUS APELLIDOS ","")
+var salida = nombre +"  "+ apellidos
+alert(salida)
